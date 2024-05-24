4aac06efd09e207dcf68002bdd1b64e72a03c1d1
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2024-05-24 15:04:18 -0500

    Filtros colocados en las PQRS
    Filtros de:
    -Cliente.
    -Asesor.
    -Cargo Generador.
    -Tipologia.
    -Rango de Fechas.
    Y se agrego boton para limpiar los filtros.

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XURXH6WO.mjs";import"./chunk-TJUXUPAO.mjs";import"./chunk-V6USGK4C.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-XPBJPHQE.mjs";import"./chunk-TJUXUPAO.mjs";import"./chunk-V6USGK4C.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
