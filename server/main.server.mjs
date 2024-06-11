7cf36a3f2d7975dfe2edf3c05a8d8c072260935e
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2024-06-11 15:48:28 -0500

    correccion de ip api

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AAANBT75.mjs";import"./chunk-TJ2ZENX5.mjs";import"./chunk-M2ZJVFHY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2GCYKDJA.mjs";import"./chunk-TJ2ZENX5.mjs";import"./chunk-M2ZJVFHY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
