33cdf9f4415a0991a85942eb3786a23d5b6915ec
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2024-07-04 13:14:25 -0500

    problemas al traer estudio credito

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ABFZYP34.mjs";import"./chunk-KAYJYQHL.mjs";import"./chunk-2MNSS3AE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ATXUYHR3.mjs";import"./chunk-KAYJYQHL.mjs";import"./chunk-2MNSS3AE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
